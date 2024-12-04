ce67c1bfa404da4f2f5a09cf05abf355413d7383
ghostbr1315 <00001114786755sp@al.educacao.sp.gov.br>
2024-12-04 18:31:05 -0300

    aula incompleta

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Culto para jovens CCB',
+    'O que é recitativo?',
+    'O recitativo é uma forma maravilhosa de manter a palavra de deus em nós '
+)
+
+criaCartao(
+    'Para casais',
+    'Para noivos, como seria uma despedida de solteiro ?',
+    'Uma despedida de solteiro é uma maneira muito importante do jovem se separar de sua vida de antes deixar o recitativo e coisas de jovem para ir e viver com seu conjugue'
+)
+
+criaCartao(
+    'Ministério',
+    'Como funciona o ministerio dessa igreja?',
+    'é uma função de caridade da igreja a qual Deus escolhe a pessoa com o conhecimento e dom certos para a obra de deus da área a qual vai agir'
+)
+
+criaCartao(
+    'caridade',
+    'Como funciona a piedade na CCB?',
+    'As pessoas doam dinheiro quando senten por parte de eus e doamos roupas, véu, cesta básica entre outros para a queles com necessidade, e se preciso arranjamos até empregos para os nessecitados e vamos de casa em casa suprindo a nessecidade de cada um'
+)
